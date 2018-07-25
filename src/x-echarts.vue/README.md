1325568f13bc63cbcc18fe228a8e70eacbe9af8a
邵月娥 <13588317944@163.com>
2018-07-25 20:18:02 +0800

    :sparkles: Add <x-echarts>

---
+++ src/x-echarts.vue/README.md
@@ -0,0 +1,190 @@
+# XECharts
+
+## 示例
+### 基本形式
+
+``` vue
+<template>
+    <x-echarts :options="options"></x-echarts>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                options: {
+                    title: {
+                        text: '每星期访问量'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data:['访问量']
+                    },
+                    xAxis: {
+                        data: ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '访问量',
+                        type: 'bar',
+                        data: [250, 50, 350, 500, 100, 80, 460]
+                    }],
+                },
+            };
+        },
+    };
+</script>
+```
+
+### 主题
+
+
+``` vue
+<template>
+    <x-echarts :options="options" theme="dark"></x-echarts>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                options: {
+                    title: {
+                        text: '每星期访问量'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data:['访问量']
+                    },
+                    xAxis: {
+                        data: ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '访问量',
+                        type: 'bar',
+                        data: [250, 50, 350, 500, 100, 80, 460]
+                    }],
+                },
+            };
+        },
+    };
+</script>
+```
+
+### 事件
+
+``` vue
+<template>
+    <x-echarts :options="options" @legendselectchanged="legendselectchanged"></x-echarts>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                options: {
+                    title: {
+                        text: '每星期访问量'
+                    },
+                    tooltip: {},
+                    legend: {
+                        data:['访问量']
+                    },
+                    xAxis: {
+                        data: ["星期一","星期二","星期三","星期四","星期五","星期六","星期日"]
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '访问量',
+                        type: 'bar',
+                        data: [250, 50, 350, 500, 100, 80, 460]
+                    }],
+                },
+            };
+        },
+        methods: {
+            legendselectchanged(params) {
+                console.log(params);
+            },
+        },
+    };
+</script>
+```
+
+## API
+### Props/Attrs
+| Prop/Attr | Type | Default | Description |
+| --------- | ---- | ------- | ----------- |
+| initOptions | Object | | 初始化时传入的附加参数 |
+| options | Object | | 配置项 |
+| theme | String,Object |  | 应用的主题 |
+| group | String,Number |  | 图表的分组 |
+| watchShallow | Boolean | `false` | 是否关闭对options的深度监听。 关闭后，options属性的变化不会触发图表更新，可用于大量数据的图表。|
+
+### Methods
+
+- setOption
+- getWidth
+- getHeight
+- getOption
+- resize
+- dispatchAction
+- convertToPixel
+- convertFromPixel
+- containPixel
+- showLoading
+- hideLoading
+- getDataURL
+- getConnectedDataURL
+- appendData
+- clear
+- dispose
+- getIsDispose
+
+### Static Methods
+
+- connect
+- disconnect
+- registerMap
+- getMap
+- registerTheme
+- graphic.clipPointsByRect
+- graphic.clipRectByRect
+
+### Events
+
+- legendselectchanged
+- legendselected
+- legendunselected
+- legendscroll
+- datazoom
+- datarangeselected
+- timelinechanged
+- timelineplaychanged
+- restore
+- dataviewchanged
+- magictypechanged
+- geoselectchanged
+- geoselected
+- geounselected
+- pieselectchanged
+- pieselected
+- pieunselected
+- mapselectchanged
+- mapselected
+- mapunselected
+- axisareaselected
+- focusnodeadjacency
+- unfocusnodeadjacency
+- brush
+- brushselected
+- rendered
+- finished
+- click
+- dblclick
+- mouseover
+- mouseout
+- mousedown
+- mouseup
+- globalout
--- src/x-echarts.vue/index.js
+++ src/x-echarts.vue/index.js
@@ -1,3 +1,192 @@
+import ECharts from 'echarts';
+import Vue from 'vue';
+
+const EVENTS = [
+    'legendselectchanged',
+    'legendselected',
+    'legendunselected',
+    'legendscroll',
+    'datazoom',
+    'datarangeselected',
+    'timelinechanged',
+    'timelineplaychanged',
+    'restore',
+    'dataviewchanged',
+    'magictypechanged',
+    'geoselectchanged',
+    'geoselected',
+    'geounselected',
+    'pieselectchanged',
+    'pieselected',
+    'pieunselected',
+    'mapselectchanged',
+    'mapselected',
+    'mapunselected',
+    'axisareaselected',
+    'focusnodeadjacency',
+    'unfocusnodeadjacency',
+    'brush',
+    'brushselected',
+    'rendered',
+    'finished',
+    'click',
+    'dblclick',
+    'mouseover',
+    'mouseout',
+    'mousedown',
+    'mouseup',
+    'globalout',
+];
+
 export default {
     name: 'x-echarts',
+    props: {
+        initOptions: Object,
+        options: Object,
+        theme: [String, Object],
+        group: [String, Number],
+        watchShallow: Boolean,
+    },
+    data() {
+        return {
+            chart: null,
+        };
+    },
+    watch: {
+        group(group) {
+            this.chart.group = group;
+        },
+        options: {
+            handler(options) {
+                !this.chart && options ? this.init() : this.setOption(options, true);
+            },
+            deep: !this.watchShallow,
+        },
+        initOptions: {
+            handler() {
+                this.refresh();
+            },
+            deep: true,
+        },
+        theme: {
+            handler() {
+                this.refresh();
+            },
+            deep: true,
+        },
+        watchShallow() {
+            this.refresh();
+        },
+    },
+    mounted() {
+        if (this.options)
+            this.init();
+    },
+    beforeDestroy() {
+        if (!this.chart)
+            return;
+        this.destroy();
+    },
+    methods: {
+        init() {
+            if (this.chart)
+                return;
+
+            const chart = ECharts.init(this.$el, this.theme, this.initOptions);
+            chart.setOption(this.options, true);
+
+            if (this.group)
+                chart.group = this.group;
+
+            EVENTS.forEach((event) => {
+                chart.on(event, (params) => {
+                    this.$emit(event, params);
+                });
+            });
+
+            this.chart = chart;
+        },
+        destroy() {
+            this.dispose();
+            this.chart = null;
+        },
+        refresh() {
+            this.destroy();
+            this.init();
+        },
+        setOption(options, notMerge, lazyUpdate) {
+            this.delegateMethod('setOption', options, notMerge, lazyUpdate);
+        },
+        getWidth() {
+            return this.delegateMethod('getWidth');
+        },
+        getHeight() {
+            return this.delegateMethod('getHeight');
+        },
+        getOption() {
+            return this.delegateMethod('getOption');
+        },
+        resize(options) {
+            this.delegateMethod('resize', options);
+        },
+        dispatchAction(payload) {
+            this.delegateMethod('dispatchAction', payload);
+        },
+        convertToPixel(finder, value) {
+            return this.delegateMethod('convertToPixel', finder, value);
+        },
+        convertFromPixel(finder, value) {
+            return this.delegateMethod('convertFromPixel', finder, value);
+        },
+        containPixel(finder, value) {
+            return this.delegateMethod('containPixel', finder, value);
+        },
+        showLoading(type, options) {
+            this.delegateMethod('showLoading', type, options);
+        },
+        hideLoading() {
+            this.delegateMethod('hideLoading');
+        },
+        getDataURL(options) {
+            return this.delegateMethod('getDataURL', options);
+        },
+        getConnectedDataURL(options) {
+            return this.delegateMethod('getConnectedDataURL', options);
+        },
+        appendData(params) {
+            this.delegateMethod('appendData', params);
+        },
+        clear() {
+            this.delegateMethod('clear');
+        },
+        dispose() {
+            this.delegateMethod('dispose');
+        },
+        getIsDispose() {
+            return this.delegateMethod('isDisposed');
+        },
+        delegateMethod(name, ...args) {
+            if (!this.chart) {
+                Vue.util.warn(`Cannot call [${name}] before the chart is initialized. Set prop [options] first.`, this);
+                return;
+            }
+            return this.chart[name](...args);
+        },
+    },
+    connect(group) {
+        ECharts.connect(group);
+    },
+    disconnect(group) {
+        ECharts.disconnect(group);
+    },
+    registerMap(mapName, geoJSON, specialAreas) {
+        ECharts.registerMap(mapName, geoJSON, specialAreas);
+    },
+    getMap(mapName) {
+        ECharts.getMap(mapName);
+    },
+    registerTheme(name, theme) {
+        ECharts.registerTheme(name, theme);
+    },
+    graphic: ECharts.graphic,
 };
